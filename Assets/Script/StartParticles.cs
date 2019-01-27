5582d48edb08b96dc2196535204032f44a0d8818
Jeffrey Lee Ye <yej9719@yahoo.com>
2019-01-26 22:50:42 -0800

    Spawn and End zones and prefab changes

---
+++ Assets/Script/StartParticles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartParticles : MonoBehaviour {
+
+	[SerializeField]
+	private float startInSeconds;
+
+	private ParticleSystem particles;
+
+	// Use this for initialization
+	void Start () {
+		particles = GetComponent<ParticleSystem>();
+		particles.Stop();
+		StartCoroutine(waitStartParticles());
+	}
+	
+	private IEnumerator waitStartParticles() {
+		float currTime = 0;
+		while (currTime < startInSeconds) {
+			currTime += Time.deltaTime;
+			yield return 0;
+		}
+		particles.Play();
+	}
+	
+}

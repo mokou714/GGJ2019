78519083475394366b55d5b23299b26f54cf244c
Jeffrey Lee Ye <yej9719@yahoo.com>
2019-01-27 12:23:54 -0800

    Prefab changes

---
+++ Assets/Script/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	[SerializeField]
+	private float degreesASecond = 10f;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newRot = transform.rotation.eulerAngles;
+		newRot.z += degreesASecond * Time.deltaTime;
+		transform.rotation = Quaternion.Euler(newRot);
+	}
+}

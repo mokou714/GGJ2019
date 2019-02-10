a1d8b29562b24ee285439c5a5b1ed0facee6de09
Zhehao Xie <zhehaox@uci.edu>
2019-02-09 21:44:35 -0800

    script fixed

--- Assets/Script/Player/spacecraft.cs
+++ Assets/Script/Player/spacecraft.cs
@@ -168,20 +168,13 @@ public class spacecraft : MonoBehaviour {
         }
 
         // Death detection
-<<<<<<< HEAD
         Vector2 viewportPos = camera.GetComponent<Camera>().WorldToViewportPoint(transform.position);
         if (energy <= 5f ||
             viewportPos.x < -0.2f ||
             viewportPos.x > 1.2f||
             viewportPos.y < -0.2f ||
             viewportPos.y > 1.2f
-=======
-        if (energy <= 10 ||
-            transform.position.x < -Constants.maxX - 10 ||
-            transform.position.x > Constants.maxX ||
-            transform.position.y < -Constants.maxY - 10 ||
-            transform.position.y > Constants.maxY
->>>>>>> a6a1bf1541dfb6a31e7f2add6504188f0d76fe0e
+
            ){
             //Application.LoadLevel(Application.loadedLevel);
             energyLoss.Stop();

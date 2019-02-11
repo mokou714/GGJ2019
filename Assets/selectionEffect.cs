3914d33ec16ace8d379a53701b6174b52eebc548
Zhehao Xie <zhehaox@uci.edu>
2019-02-10 16:39:47 -0800

    levelPlanet

--- Assets/selectionEffect.cs
+++ Assets/selectionEffect.cs
@@ -8,9 +8,11 @@ public class selectionEffect : MonoBehaviour {
     public float largeScale;
     Vector3 originScale;
 
+  
+
 	// Use this for initialization
 	void Start () {
-        originScale = transform.localScale;
+        originScale = transform.GetChild(0).localScale;
 	
 	}
 	
@@ -20,16 +22,56 @@ public class selectionEffect : MonoBehaviour {
 	}
 
 
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    IEnumerator fadeIn() {
+        float currentAlpha = 0f;
+
+        while (currentAlpha < 1f)
+        {
+            currentAlpha = GetComponent<SpriteRenderer>().color.a;
+            float newAlpha = currentAlpha + 0.05f;
+            GetComponent<SpriteRenderer>().color = new Color(255f,255f,255f,newAlpha);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    IEnumerator fadeOut()
+    {
+        float currentAlpha = 1f;
+
+        while (currentAlpha > 0f)
+        {
+            currentAlpha = GetComponent<SpriteRenderer>().color.a;
+            float newAlpha = currentAlpha - 0.05f;
+            GetComponent<SpriteRenderer>().color = new Color(255f, 255f, 255f, newAlpha);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+
+
+
     private void OnMouseEnter()
     {
-        transform.localScale = new Vector3(largeScale, largeScale, 0);
+        transform.GetChild(0).localScale = new Vector3(largeScale, largeScale, 0f);
+
+
     }
 
     private void OnMouseExit()
     {
-        transform.localScale = originScale;
+
+        transform.GetChild(0).localScale = originScale;
+
     }
-
-
 
 }

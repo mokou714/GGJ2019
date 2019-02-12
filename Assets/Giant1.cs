1a0b744c5158cab1eab907fa4d7feb40bfdcc788
Zhehao Xie <zhehaox@uci.edu>
2019-02-11 17:56:10 -0800

    giant1

---
+++ Assets/Giant1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Giant1 : MonoBehaviour {
+
+
+  
+    public float endX;
+    public GameObject player;
+    public float moveSpeed;
+    public bool moving;
+
+
+
+	// Use this for initialization
+	void Start () {
+        moving = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (moving) {
+            Vector3 newPos = transform.position;
+            Vector3 newPos2 = player.transform.position;
+            newPos.x += moveSpeed * Time.deltaTime;
+            newPos2.x += moveSpeed * Time.deltaTime;
+
+
+            if (newPos.x > endX)
+            {
+                moving = false;
+                newPos.x = endX;
+                player.transform.GetChild(0).GetComponent<spacecraft>().enabled = true;
+            }
+            transform.position = newPos;
+            player.transform.position = newPos2;
+        }
+    }
+}

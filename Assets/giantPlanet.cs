baed4f3248d8eb8b2a4c5047e3391864c217e115
Zhehao Xie <zhehaox@uci.edu>
2019-02-11 17:20:49 -0800

    giantPlanetLevel

---
+++ Assets/giantPlanet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class giantPlanet : MonoBehaviour {
+
+
+    public float moveSpeed;
+    public Vector3 destinationPos;
+    public bool moving;
+
+    public GameObject dustPlanet;
+
+
+	// Use this for initialization
+	void Start () {
+        moving = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (moving) {
+            Vector3 newPos = transform.position;
+            newPos.x -= Time.deltaTime * moveSpeed;
+
+            Vector3 _newPos = dustPlanet.transform.position;
+            _newPos.x -= Time.deltaTime * moveSpeed;
+
+
+            if (newPos.x <= 20f || dustPlanet.GetComponent<dustPlanet>().thePlayerOnPlanet != null) {
+                newPos.x = 20f;
+                moving = false;
+            }
+            transform.position = newPos;
+            dustPlanet.transform.position = _newPos;
+
+            
+        }
+    }
+}
